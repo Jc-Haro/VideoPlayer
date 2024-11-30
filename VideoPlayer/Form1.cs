7efe03b95b42c702fc79242c0a7876b2687e9262
Juan Haro <jcharomo@gmail.com>
2024-11-29 19:42:32 -0600

    Add box filling logic

--- VideoPlayer/Form1.cs
+++ VideoPlayer/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,38 @@ namespace VideoPlayer
 {
     public partial class Form1 : Form
     {
+        int mondongocount = 0;
+        private static string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\videos.json";
+
+
         public Form1()
         {
             InitializeComponent();
+            FillVideosBox();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            VideoData videoData = new VideoData("title");
+            SaveData data = new SaveData(videoData);
+        }
+
+        private void FillVideosBox()
+        {
+            List<VideoData> list = new List<VideoData>();
+            if (File.Exists(filePath))
+            {
+                GetData data = new GetData();
+                string jsonIn = data.GetDataFromFile(filePath);
+                list = JsonSerializer.Deserialize<List<VideoData>>(jsonIn);
+                for (int i = 0; i < list.Count; i++)
+                {
+                    textBox1.Text += list[i].Title + " "  + Environment.NewLine;
+                    mondongocount++;
+                    textBox1.SelectionStart = textBox1.TextLength;
+                }
+            }
+            
         }
     }
 }
---
+++ VideoPlayer/GetData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VideoPlayer
+{
+    internal class GetData
+    {
+        public string GetDataFromFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return File.ReadAllText(filePath);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
